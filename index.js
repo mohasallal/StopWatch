3fa2591b2cfaa2751a0ca08f9f5af64d44946c72
Mohammad Alsallal <malslal787@gmail.com>
2024-07-28 19:40:00 +0300

    StopWatch

---
+++ index.js
@@ -0,0 +1,51 @@
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let isRunning = false;
+let elapsedTime = 0;
+
+start.addEventListener("click", () => {
+  if (!isRunning) {
+    startTime = Date.now() - elapsedTime;
+    timer = setInterval(update, 10);
+    isRunning = true;
+  }
+});
+
+stop.addEventListener("click", () => {
+  if (isRunning) {
+    clearInterval(timer);
+    elapsedTime = Date.now() - startTime;
+    isRunning = false;
+  }
+});
+
+reset.addEventListener("click", () => {
+  if (isRunning) {
+    clearInterval(timer);
+    elapsedTime = 0;
+    startTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00";
+  }
+});
+
+function update() {
+  const CurrentTime = Date.now();
+  elapsedTime = CurrentTime - startTime;
+
+  let Hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+  let min = Math.floor((elapsedTime / (1000 * 60)) % 60);
+  let sec = Math.floor((elapsedTime / 1000) % 60);
+  let milsec = Math.floor((elapsedTime % 1000) / 10);
+
+  display.textContent = `${Hours.toString().padStart(2, "0")}:${min
+    .toString()
+    .padStart(2, "0")}:${sec.toString().padStart(2, "0")}:${milsec
+    .toString()
+    .padStart(2, "0")}`;
+}
